f81b1be64251b3c37785d1e71332e85b83ebea66
Shobhit Kumar <87258878+kumar-shobhit@users.noreply.github.com>
2023-10-20 17:00:51 +0530

    First_Commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const gallery = document.querySelector(".container");
+const url = "https://pbivizedit.com/api/visuals";
+
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data.items.forEach((item) => {
+      const img = document.createElement("img");
+      img.src = item.imagePath;
+      img.alt = item.name;
+      const description = document.createElement("p");
+      description.textContent = item.description;
+
+      const galleryItem = document.createElement("div");
+      galleryItem.className = "innerdata";
+      galleryItem.appendChild(img);
+      galleryItem.appendChild(description);
+
+      gallery.appendChild(galleryItem);
+    });
+  })
+  .catch(error=>console.error("error", error));
+
+const searchFun = () => {
+    let filter = document.getElementById('input').value.toUpperCase();
+
+    let visuals = document.getElementById('visuals');
+
+    let dec = visuals.getElementsByClassName('innerdata');
+
+    for(var i =0;i<dec.length;i++){
+        let a = dec[i].getElementsByTagName('p')[0];
+
+        if(a){
+            let avalue = a.textContent || a.innerHTML;
+
+            if(avalue.toUpperCase().indexOf(filter) > -1){
+                    dec[i].style.display = "";
+        }
+        else{
+            dec[i].style.display = "none";
+        }
+    }
+
+
+
+
+}
+};
